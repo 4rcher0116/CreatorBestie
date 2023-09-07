a07dfac8950ffce0d420a3b20fe93964162b3f9b
4rcher0116 <4rcher0116@gmail.com>
2023-09-07 17:30:14 -0500

    update logo, further improvements on dashboard and sidebar

--- creator-bestie/components/Sidebar.tsx
+++ creator-bestie/components/Sidebar.tsx
@@ -1,9 +1,32 @@
 "use client";
 
+import { cn } from "@/lib/utils";
+import { Montserrat } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
+
+const montserrat = Montserrat({ 
+    weight: "600",
+    subsets:["latin"]
+});
+
 const Sidebar = () => {
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
-        
+        <div className="px-3 py-2 flex-1">
+            <Link href="/dashboard" className="flex items-center pl-3 mb-14">
+                <div className="relative w-36 h-8 mr-4">
+                    <Image 
+                        fill
+                        alt="logo"
+                        src="/logo.png"
+                    />
+                </div>
+                {/* <h1 className={cn("text-2xl font-bold")}>
+                    Canyon AI
+                </h1> */}
+            </Link>
+        </div>
     </div>
   )
 }
